c0f6bec6cb67394c031728963d3af81886d2b26e
chinasemb <1032944759@qq.com>
2019-09-16 14:57:25 +0800

    init

---
+++ 01/pinduoduo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pinduoduo';
+  tabs = ['x', 'y']
+}
